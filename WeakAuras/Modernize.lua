3ef15e0fae23bedaaaf37984628b1f12864c4fe4
NoM0Re <patrick_S_99@gmx.de>
2025-05-09 13:41:32 +0200

    add flavor functions, remove Deathknight from Classic/TBC, small parity changes

--- WeakAuras/Modernize.lua
+++ WeakAuras/Modernize.lua
@@ -292,7 +292,7 @@ function Private.Modernize(data, oldSnapshot)
   -- Version 18 was a migration for stance/form trigger, but deleted later because of migration issue
 
   -- Version 19 were introduced in July 2019 in BfA
-  if WeakAuras.isAwesomeEnabled() then
+  if WeakAuras.IsAwesomeEnabled() then
     if data.internalVersion < 19 then
       if data.triggers then
         for triggerId, triggerData in ipairs(data.triggers) do
