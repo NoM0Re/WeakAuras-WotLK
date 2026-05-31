bba374df843e573adb56917884cd97bd2af32261
NoM0Re <1629787+NoM0Re@users.noreply.github.com>
2026-05-31 18:35:14 +0200

    Reapply "Fix rename lua error"
    This reverts commit ecf6bfd00f9dadc1a5614a9df69478075a0abe4d.

--- WeakAuras/WeakAuras.lua
+++ WeakAuras/WeakAuras.lua
@@ -2177,10 +2177,11 @@ function WeakAuras.Rename(data, newid)
 
   Private.ProfileRenameAura(oldid, newid);
 
-  -- TODO: This should not be necessary
-  WeakAuras.Add(data)
 
   Private.callbacks:Fire("Rename", data.uid, oldid, newid)
+
+  -- TODO: This should not be necessary
+  WeakAuras.Add(data)
 end
 
 function Private.Convert(data, newType)
